8d7372b3d5313c4b20cf3fb1122077842e5d3456
Salvatore Castelbuono <castelbuonosalvatore@gmail.com>
2020-02-12 11:06:15 -0600

    Update VL6180X.js

--- devices/VL6180X.js
+++ devices/VL6180X.js
@@ -172,7 +172,7 @@ VL6180X.prototype.readRange = function(cb){
 
 };
 
-VL6180X.prototype.readRangeRC(triesLeft,rcTimeoutTime,rc,cb){
+VL6180X.prototype.readRangeRC = function(triesLeft,rcTimeoutTime,rc,cb){
   // ready check for measurement - and start measurement
   
   var s = this.read8(C.VL6180X_REG_RESULT_RANGE_STATUS);
@@ -194,7 +194,7 @@ VL6180X.prototype.readRangeRC(triesLeft,rcTimeoutTime,rc,cb){
   }                                                                         // alternative: cb(32|s); combined err and undefined for val
 };
 
-VL6180X.prototype.readRangeCC(triesLeft,ccTimeoutTime,cc,cb) {
+VL6180X.prototype.readRangeCC = function(triesLeft,ccTimeoutTime,cc,cb) {
   // completion check for measurement - and read measurement and clear interrupt
   
   var s = this.read8(C.VL6180X_REG_RESULT_INTERRUPT_STATUS_GPIO);
