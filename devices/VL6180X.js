4f5d9f69d55dee7990515aeb4e765e0c526a9b6f
Salvatore Castelbuono <castelbuonosalvatore@gmail.com>
2020-02-12 17:28:57 -0600

    Update VL6180X.js

--- devices/VL6180X.js
+++ devices/VL6180X.js
@@ -50,14 +50,14 @@ function VL6180X(i2c, options) {
      this.ad = this.options.address;
      this.i2c.writeTo(C.VL6180X_DEFAULT_I2C_ADDR, C.VL6180X_REG_I2C_ADDR, this.ad);
     }
-    if (this.read8(C.VL6180X_REG_IDENTIFICATION_MODEL_ID) != 0xB4) {
-      console.log(this.read(C.VL6180X_REG_IDENTIFICATION_MODEL_ID));
+    if (this.read8(C.VL6180X_REG_IDENTIFICATION_MODEL_ID) != 0xB4){
+      console.log(this.read8(C.VL6180X_REG_IDENTIFICATION_MODEL_ID));
     }
-    if (this.read(C.VL6180X_REG_SYSTEM_FRESH_OUT_OF_RESET) == 0x01) {
+    if (this.read(C.VL6180X_REG_SYSTEM_FRESH_OUT_OF_RESET) == 0x01){
       console.log("Load settings");
       this.loadSettings();
     }
-        console.log("No Load settings");
+    console.log("No Load settings");
     this.write8(C.VL6180X_REG_SYSTEM_FRESH_OUT_OF_RESET, 0x00);
 }
 
